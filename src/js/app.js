89e85f717c83d0fe84ea7ba39cd497b448a5c397
Francisco Angulo <QK148GL@SA.EY.NET>
2017-11-14 16:02:37 -0600

    ajustes al app

--- src/js/app.js
+++ src/js/app.js
@@ -21,56 +21,39 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('SupplyChainRegistry.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var SupplyChainRegistry = data;
-      App.contracts.SupplyChainRegistry = TruffleContract(SupplyChainRegistry);
-
-      // Set the provider for our contract
-      App.contracts.SupplyChainRegistry.setProvider(App.web3Provider);
-      return App.getActor();
-    });
-
-    $.getJSON('ProductDatabase.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var ProductDatabase = data;
-      App.contracts.ProductDatabase = TruffleContract(ProductDatabase);
-
-      // Set the provider for our contract
-      App.contracts.ProductDatabase.setProvider(App.web3Provider);
-    });
-
-    $.getJSON('Product.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var Product = data;
-      App.contracts.Product = TruffleContract(Product);
-
-      // Set the provider for our contract
-      App.contracts.Product.setProvider(App.web3Provider);
-    });
-
     $.getJSON('ProductFactory.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var ProductFactory = data;
-      App.contracts.ProductFactory = TruffleContract(ProductFactory);
-
-      // Set the provider for our contract
+      App.contracts.ProductFactory = TruffleContract(data);
       App.contracts.ProductFactory.setProvider(App.web3Provider);
     });
+    $.getJSON('SupplyChainRegistry.json', function(data) {
+      var SupplyChainRegistry = data;
+      App.contracts.SupplyChainRegistry = TruffleContract(SupplyChainRegistry);
+      App.contracts.SupplyChainRegistry.setProvider(App.web3Provider);
+
+      $.getJSON('ProductDatabase.json', function(data) {
+        App.contracts.ProductDatabase = TruffleContract(data);
+        App.contracts.ProductDatabase.setProvider(App.web3Provider);
+        
+        $.getJSON('Product.json', function(data) {
+          App.contracts.Product = TruffleContract(data);
+          App.contracts.Product.setProvider(App.web3Provider);
+        });
+      });
+
+      return App.getActor();
+    });   
 
     return App.bindEvents();
   },
 
-  
-  
   bindEvents: function() {
     $(document).on('click', '#btn-register', App.registerActor);
     $(document).on('click', '#btn-add-package', App.addProduct);
   },
 
-  getActor: function(account) {
+  getActor: function(adopters, account) {
     var ci;
-
+    
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
@@ -81,24 +64,21 @@ App = {
         ci = instance;
         return ci.getActor(account);
       }).then(function(data) {
-        if (data[1] !== "") {
-          $("#actor_name").text(data[1]);
-          var type="";
-          App.productDatabase = data[2];
-          switch(data[0].e){
-            case 0:
-              type="Manufacturer";
-              break;
-            case 1: 
-              type="Shipper";
-              break;
-            case 2: 
-              type="Distributor";
-              break;
-            case 3: 
-              type="Retailer";
-              break;
-          }
+        App.productDatabase = data[2];
+        var type="";
+        switch(data[0].e){
+          case 0:
+            type="Manufacturer";
+            break;
+          case 1: 
+            type="Shipper";
+            break;
+          case 2: 
+            type="Distributor";
+            break;
+          case 3: 
+            type="Retailer";
+            break;
         }
         if (type !== undefined) {
           $("#welcome-message").text("Welcome " + data[1] + "!, you are registered as " + type + ".");
@@ -113,6 +93,8 @@ App = {
   registerActor: function(event) {
     event.preventDefault();
 
+    //var petId = parseInt($(event.target).data('id'));
+
     var ci;
 
     web3.eth.getAccounts(function(error, accounts) {
@@ -125,6 +107,7 @@ App = {
       App.contracts.SupplyChainRegistry.deployed().then(function(instance) {
         ci = instance;
         
+        // Execute adopt as a transaction by sending account
         var name = $("#name").val();
         var type = $("#type").val();
         return ci.registerActor(type,name, {from: account});
@@ -136,15 +119,40 @@ App = {
     });
   },
 
+  addProduct: function(event){
+    event.preventDefault();
+    App.contracts.ProductDatabase.at(App.productDatabase).then(function(instance) {
+      var event = instance.OnAddProductEvent();
+      event.watch(function(error, response){
+        console.log(response);
+        App.getProducts();
+        $('#PackageModal').modal('hide');
+        event.stopWatching();
+      });
+    });
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.ProductFactory.deployed().then(function(instance) {        
+        // Execute adopt as a transaction by sending account
+        var name = $("#package").val();
+        return instance.createProduct(name, {from: account});
+      }).then(function(result) {
+        
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
   getProducts: function(){
     App.contracts.ProductDatabase.at(App.productDatabase).then(function(instance) {
-      instance.OnAddProductEvent().watch(function(error, response){
-        console.log(error);
-        console.log(response);
-      });
       return instance.getCount();
     }).then(function(response) {
-      console.log(response);
       $("#package-list tbody").empty();
       for (i = 0; i < response.c[0]; i ++) {
         (function(x){
@@ -153,8 +161,7 @@ App = {
           }).then(address=>{
             App.contracts.Product.at(address).then(instance3=>{
               return instance3.getState();            
-           }).then(data=>{
-              console.log(data);
+            }).then(data=>{
               var tr = $('<tr></tr>');
               var link = $('<td style="word-wrap: break-word; max-width: 250px;"></td>');
               link.html($('<a data-toggle="modal" data-target="#HistoryModal"></a>').html(address));
@@ -163,7 +170,6 @@ App = {
               tr.append($("<td></td>").html(data[2]));
               tr.append($("<td></td>").html(data[1].c[0]));
               $("#package-list tbody").append(tr);
-              console.log(tr);
             });
           });
         })(i);
@@ -171,53 +177,7 @@ App = {
     }).catch(function(err) {
       console.log(err.message);
     });
-  },
-
-  // DONE
-  addProduct: function(event) {
-    
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log("error 1:", error);
-      }
-      var account = accounts[0];
-      App.contracts.ProductFactory.deployed().then(function(instance) {
-        var name = $("#package").val();
-        return instance.createProduct(name, {from: account});
-      }).then(function(result) {
-        return App.getActor();
-      }).catch(function(err) {
-        console.log("error 2", err.message)
-      });
-    });
-  },
-
-  // redo 
-  changeProductStatus: function(event) {
-    event.preventDefault();
-
-    var ci;
-
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
-      }
-
-      var account = accounts[0];
-
-      App.contracts.PackageTraker.deployed().then(function(instance) {
-        ci = instance;
-      
-        var packageId = $("#packageId").val();
-        var status = $("#status").val();
-        return ci.changePackageStatus(packageId,status, {from: account});
-      }).then(function(result) {
-        return App.getPackages();
-      }).catch(function(err) {
-        console.log(err.message);
-      });
-    });
-  }  
+  }
 
 };
 
