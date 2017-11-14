e7ec5dd434ca19afa0fdace224a9db3b0027f22b
Francisco Angulo <QK148GL@SA.EY.NET>
2017-11-14 16:09:48 -0600

    ajustes al codigo

--- migrations/3_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var SupplyChainRegistry = artifacts.require("./SupplyChainRegistry.sol");
-var ProductFactory = artifacts.require("./ProductFactory.sol");
-
-module.exports = function(deployer){
-    deployer.deploy(SupplyChainRegistry);
-    deployer.deploy(SupplyChainRegistry).then(function() {
-        return deployer.deploy(ProductFactory, SupplyChainRegistry.address);
-      });
-}
